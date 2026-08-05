2a243f9c15dd614486de6a544f6edf2fb6b6f349
Carlo Deutschmann <deutschmanncarlo@gmail.com>
2026-08-05 20:02:54 +0200

    do not write to stderr from library code

--- src/response.rs
+++ src/response.rs
@@ -163,7 +163,6 @@ fn parse_multipart(body: &[u8], boundary: &str) -> Result<Vec<Parameter>> {
             }
             multipart::server::ReadEntryResult::End(_) => return Ok(parameters),
             multipart::server::ReadEntryResult::Error(_, error) => {
-                eprintln!("Ran into error during reading of multipart: {}", error);
                 return Err(Error::from(error));
             }
         };
